662af8ad340b2dcb9944bbf5aae9149bec7c2827
hkyang <devp.lulluli@gmail.com>
2021-05-11 10:26:08 +0900

    api 작업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import VideoList from './components/video_list/video_list.jsx';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [popularVideos, setPopularVideos] = useState([]);
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+        };
+
+        fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDFNmDcazUr-rqvQchPr2cZxw7JVUzQriE', requestOptions)
+            .then(response => response.text())
+            .then(result => setPopularVideos(JSON.parse(result).items))
+            .catch(error => console.log('error', error));
+    }, []);
+    return <VideoList className='App' videos={popularVideos}></VideoList>;
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const videoItem = props => <h1>{props.video.snippet.title}</h1>;
+
+export default videoItem;
